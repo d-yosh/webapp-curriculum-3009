5b1011a232dc1d90f36b947e4064d26555d532ae
d-yosh <daicedaice87@gmail.com>
2020-01-20 14:45:34 +0900

    MustResume を実装

--- src/main/scala/MustResume.scala
+++ src/main/scala/MustResume.scala
@@ -1,6 +1,12 @@
-import akka.actor.{Actor, Props}
+import akka.actor.SupervisorStrategy.Resume
+import akka.actor.{Actor, ActorRef, ActorSystem, Inbox, OneForOneStrategy, Props}
 
+import scala.concurrent.duration._
 class ParentActor extends Actor {
+  override val supervisorStrategy = OneForOneStrategy() {
+    case _: Exception => Resume
+  }
+
   def receive = {
     case p: Props => sender() ! context.actorOf(p)
   }
@@ -8,11 +14,20 @@ class ParentActor extends Actor {
 
 class ChildActor extends Actor {
   def receive = {
-    case s: String => println(s)
+    case s: String    => println(s)
     case e: Exception => throw e
   }
 }
 
 object MustResume extends App {
+  val system = ActorSystem("mustResume")
+  val inbox = Inbox.create(system)
+  implicit val sender = inbox.getRef()
 
+  val parent = system.actorOf(Props[ParentActor], "parent")
+  parent ! Props[ChildActor]
+  val child = inbox.receive(5.seconds).asInstanceOf[ActorRef]
+  child ! new RuntimeException
+
+  Thread.currentThread().join()
 }
