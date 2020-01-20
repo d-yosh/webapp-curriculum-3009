4b8cce97f54ba6bb012b672e48be91f07bb5655b
d-yosh <daicedaice87@gmail.com>
2020-01-20 14:48:17 +0900

    PoisonPillStudy を実装

--- src/main/scala/PoisonPillStudy.scala
+++ src/main/scala/PoisonPillStudy.scala
@@ -1,3 +1,11 @@
-object PoisonPillStudy extends App {
+import akka.actor.{ActorSystem, PoisonPill, Props}
 
+object PoisonPillStudy extends App {
+  val system = ActorSystem("poisonPillStudy")
+  val child = system.actorOf(Props[ChildActor], "child")
+  child ! "test"
+  child ! PoisonPill
+  child ! "hogehoge"
+
+  Thread.currentThread().join()
 }
